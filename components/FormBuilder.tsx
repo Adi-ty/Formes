d293c9ef774f3d96c6a4bb33ff6ccad8e06b802f
Aditya <adityasinghboss1234@gmail.com>
2023-12-28 19:41:05 +0530

    form builder layout

---
+++ components/FormBuilder.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Form } from "@prisma/client";
+import SaveFormBtn from "./SaveFormBtn";
+import PreviewDialogBtn from "./PreviewDialogBtn";
+import PublishFormBtn from "./PublishFormBtn";
+
+function FormBuilder({ form }: { form: Form }) {
+  return (
+    <main className="flex flex-col w-full">
+      <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
+        <h2 className="truncate font-medium">
+          <span className="text-muted-foreground mr-2">Form:</span>
+          {form.name}
+        </h2>
+        <div className="flex items-center gap-2">
+          <PreviewDialogBtn />
+          {!form.published && (
+            <>
+              <SaveFormBtn />
+              <PublishFormBtn />
+            </>
+          )}
+        </div>
+      </nav>
+      <div className="flex w-full flex-grow items-center justify-center relative overflow-y-auto h-[200px] bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]"></div>
+    </main>
+  );
+}
+
+export default FormBuilder;
---
+++ components/PublishFormBtn.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "./ui/button";
+
+function PublishFormBtn() {
+  return (
+    <Button className="gap-2 text-white bg-gradient-to-r from-rose-400 to-teal-400">
+      Publish
+    </Button>
+  );
+}
+
+export default PublishFormBtn;
