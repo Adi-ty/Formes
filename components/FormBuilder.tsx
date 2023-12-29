732a599c14b89a05c2609456a78b0a6070b1aa28
Aditya <adityasinghboss1234@gmail.com>
2023-12-29 13:08:43 +0530

    mouse and touch sensors

--- components/FormBuilder.tsx
+++ components/FormBuilder.tsx
@@ -1,16 +1,37 @@
 "use client";
 
+import {
+  DndContext,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
 import { Form } from "@prisma/client";
-import SaveFormBtn from "./SaveFormBtn";
+import Designer from "./Designer";
+import DragOverlayWrapper from "./DragOverlayWrapper";
 import PreviewDialogBtn from "./PreviewDialogBtn";
 import PublishFormBtn from "./PublishFormBtn";
-import Designer from "./Designer";
-import { DndContext } from "@dnd-kit/core";
-import DragOverlayWrapper from "./DragOverlayWrapper";
+import SaveFormBtn from "./SaveFormBtn";
 
 function FormBuilder({ form }: { form: Form }) {
+  const mouseSensor = useSensor(MouseSensor, {
+    activationConstraint: {
+      distance: 10, // 10px
+    },
+  });
+
+  const touchSensor = useSensor(TouchSensor, {
+    activationConstraint: {
+      delay: 300,
+      tolerance: 5,
+    },
+  });
+
+  const sensors = useSensors(mouseSensor, touchSensor);
+
   return (
-    <DndContext>
+    <DndContext sensors={sensors}>
       <main className="flex flex-col w-full">
         <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
           <h2 className="truncate font-medium">
