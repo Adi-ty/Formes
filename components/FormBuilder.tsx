a5eb66ecb6ba53247425af6681ec16ceed9fcd19
Aditya <adityasinghboss1234@gmail.com>
2023-12-30 13:28:26 +0530

    form publish

--- components/FormBuilder.tsx
+++ components/FormBuilder.tsx
@@ -16,6 +16,12 @@ import PublishFormBtn from "./PublishFormBtn";
 import SaveFormBtn from "./SaveFormBtn";
 import useDesigner from "./hooks/useDesigner";
 import { ImSpinner2 } from "react-icons/im";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { toast } from "./ui/use-toast";
+import Link from "next/link";
+import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
+import Confetti from "react-confetti";
 
 function FormBuilder({ form }: { form: Form }) {
   const { setElements } = useDesigner();
@@ -51,6 +57,61 @@ function FormBuilder({ form }: { form: Form }) {
     );
   }
 
+  const shareUrl = `${window.location.origin}/submit/${form.shareURL}`;
+
+  if (form.published) {
+    return (
+      <>
+        <Confetti
+          width={window.innerWidth}
+          height={window.innerHeight}
+          recycle={false}
+          numberOfPieces={1000}
+        />
+        <div className="flex flex-col items-center justify-center h-full w-full">
+          <div className="max-w-md">
+            <h1 className="text-center text-4xl font-bold text-primary border-b pb-2 mb-10 text-white">
+              🎉 Form Published 🎉
+            </h1>
+            <h2 className="text-2xl">Share this form</h2>
+            <h3 className="text-xl text-muted-foreground border-b pb-10">
+              Anyone with the link can view and submit the form.
+            </h3>
+            <div className="my-4 flex flex-col gap-2 items-center w-full border-b pb-4">
+              <Input className="w-full" readOnly value={shareUrl} />
+              <Button
+                className="mt-2 w-full"
+                onClick={() => {
+                  navigator.clipboard.writeText(shareUrl);
+                  toast({
+                    title: "Copied!",
+                    description: "Link copied to clipboard.",
+                  });
+                }}
+              >
+                Copy Link
+              </Button>
+            </div>
+            <div className="flex justify-between">
+              <Button variant={"link"} asChild>
+                <Link href={"/"} className="gap-2">
+                  <BsArrowLeft />
+                  Go Back Home
+                </Link>
+              </Button>
+              <Button variant={"link"} asChild>
+                <Link href={`/forms/${form.id}`} className="gap-2">
+                  Form Details
+                  <BsArrowRight />
+                </Link>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+
   return (
     <DndContext sensors={sensors}>
       <main className="flex flex-col w-full">
@@ -64,7 +125,7 @@ function FormBuilder({ form }: { form: Form }) {
             {!form.published && (
               <>
                 <SaveFormBtn id={form.id} />
-                <PublishFormBtn />
+                <PublishFormBtn id={form.id} />
               </>
             )}
           </div>
--- components/PublishFormBtn.tsx
+++ components/PublishFormBtn.tsx
@@ -1,11 +1,75 @@
-import React from "react";
+import { PublishForm } from "@/actions/form";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import { FaIcons } from "react-icons/fa";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./ui/alert-dialog";
 import { Button } from "./ui/button";
+import { toast } from "./ui/use-toast";
 
-function PublishFormBtn() {
+function PublishFormBtn({ id }: { id: number }) {
+  const [loading, startTransition] = useTransition();
+  const router = useRouter();
+
+  async function publishForm() {
+    try {
+      await PublishForm(id);
+      toast({
+        title: "Success",
+        description: "Your form is now available to receive submissions.",
+      });
+      router.refresh();
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "Something went wrong!",
+        variant: "destructive",
+      });
+    }
+  }
+
   return (
-    <Button className="gap-2 text-white bg-gradient-to-r from-rose-400 to-teal-400">
-      Publish
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button className="gap-2 text-white bg-gradient-to-r from-rose-400 to-teal-400">
+          Publish
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            After publishing the form you will no longer be able to edit this
+            form. <br />
+            <span className="font-medium">
+              By publishing this form you will make it available to the public
+              and you will be able to collect submissions.
+            </span>
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            disabled={loading}
+            onClick={(e) => {
+              e.preventDefault();
+              startTransition(publishForm);
+            }}
+          >
+            Proceed {loading && <FaIcons className="animate-spin" />}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
 
