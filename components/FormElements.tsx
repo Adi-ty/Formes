3421eae6fe53f820265da4657b282cac5f9f6738
Aditya <adityasinghboss1234@gmail.com>
2024-01-06 04:56:51 +0530

    Added title layout element

--- components/FormElements.tsx
+++ components/FormElements.tsx
@@ -1,6 +1,7 @@
 import { TextFieldFormElement } from "./fields/TextField";
+import { TitleFieldFormElement } from "./fields/TitleField";
 
-export type ElementsType = "TextField";
+export type ElementsType = "TextField" | "TitleField";
 
 export type SubmitFunction = (key: string, value: string) => void;
 
@@ -41,4 +42,5 @@ type FormElementsType = {
 };
 export const FormElements: FormElementsType = {
   TextField: TextFieldFormElement,
+  TitleField: TitleFieldFormElement,
 };
